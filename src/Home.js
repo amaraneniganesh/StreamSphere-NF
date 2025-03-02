f823f7a27016a09649af568072f59678f87622a9
saiganesh1745 <saig949218@gmail.com>
2025-03-02 11:28:34 +0530

    v4a

--- src/Home.js
+++ src/Home.js
@@ -109,7 +109,7 @@ const heroMovies = [
 {
   "title": "Sankranthiki Vasthunam",
   "poster": "https://m.media-amazon.com/images/M/MV5BM2NiMzA5MjctZTJjNC00ZWRmLTgzMTctODdhYzFmN2VlY2FhXkEyXkFqcGc@._V1_.jpg",
-  "hoverPoster": "https://www.shmoti.com/ImageFiles/Image_555x271/20250107_Banner_SankrathikiVastunamaS2E20250107.jpg",
+  "hoverPoster": "https://pbs.twimg.com/media/GgXORvlbcAAcFMF?format=jpg&name=900x900",
   "link": "https://drive.google.com/file/d/1GbtZwbu-PW52O-U9Bu2bkxB7TCXQfMn3/view",
   "description":"When a renowned tech entrepreneur is kidnapped upon returning to India, a police officer recruits her ex-boyfriend, a former cop now living a quiet family life, to assist in the rescue mission.",
   "cast": {
